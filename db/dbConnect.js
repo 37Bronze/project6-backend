eb8a6e0b5f5a2be3efb860e05e7277fd5501cbd0
37Bronze <ndinhdong.1808@gmail.com>
2024-05-19 12:15:41 +0700

    first commit

---
+++ db/dbConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function dbConnect() {
+  mongoose
+    .connect(process.env.URL)
+    .then(() => {
+      console.log("Connected MongoDB");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3030;
+const UserRouter = require("./router/UserRouter");
+const PhotoRouter = require("./router/PhotoRouter");
+const dbConnect = require("./db/dbConnect");
+
+app.use(cors());
+dbConnect();
+
+app.use(UserRouter);
+app.use("/photos", PhotoRouter);
+
+app.get("/", (req, res) => {
+  res.send("back end pr6");
+});
+
+app.listen(port, function () {
+  console.log(port);
+});
